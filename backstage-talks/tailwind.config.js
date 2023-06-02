8c80885b7044e2cc8a1c287849c8140ac4dd3204
Felipe Alves <profelipealves@outlook.com>
2023-06-02 07:04:38 -0300

    configura tailwindcss

--- backstage-talks/tailwind.config.js
+++ backstage-talks/tailwind.config.js
@@ -2,20 +2,38 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
-  content: ["./index.html",
-  "./src/**/*.{js,ts,jsx,tsx}",],
+  content: ['./pages/**/*.{html,js}',
+  './components/**/*.{html,js}',],
   theme: {
-    extend: {
-      colors: {
-        background: '#09090a',
-      },
-      gridTemplateRows: {
-        7: 'repeat(7, minmax(0, 1fr))',
-      },
-      fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
-      },
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
     },
-  },
-  // ...
+    colors: {
+      'blue': '#1fb6ff',
+      'purple': '#7e5bef',
+      'pink': '#ff49db',
+      'orange': '#ff7849',
+      'green': '#13ce66',
+      'yellow': '#ffc82c',
+      'gray-dark': '#273444',
+      'gray': '#8492a6',
+      'gray-light': '#d3dce6',
+    },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    extend: {
+      spacing: {
+        '128': '32rem',
+        '144': '36rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      }
+    }
+  }
 }
