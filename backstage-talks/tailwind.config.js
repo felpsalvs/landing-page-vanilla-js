714b1febf6302b2a38b591024bfb59d9a75a8de7
Felipe Alves <profelipealves@outlook.com>
2023-06-02 09:54:41 -0300

    chore: atualizando todas dependencias

--- backstage-talks/tailwind.config.js
+++ backstage-talks/tailwind.config.js
@@ -2,10 +2,9 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
-  content: ['./pages/**/*.{html,js}',
+  content: ['./src/**/*.{html,js}',
   './components/**/*.{html,js}',],
   theme: {
-<<<<<<< HEAD
     extend: {
       colors: {
         background: '#090a',
@@ -19,37 +18,4 @@ module.exports = {
     },
   },
   plugins: [require('@tailwindcss/forms')]
-=======
-    screens: {
-      sm: '480px',
-      md: '768px',
-      lg: '976px',
-      xl: '1440px',
-    },
-    colors: {
-      'blue': '#1fb6ff',
-      'purple': '#7e5bef',
-      'pink': '#ff49db',
-      'orange': '#ff7849',
-      'green': '#13ce66',
-      'yellow': '#ffc82c',
-      'gray-dark': '#273444',
-      'gray': '#8492a6',
-      'gray-light': '#d3dce6',
-    },
-    fontFamily: {
-      sans: ['Graphik', 'sans-serif'],
-      serif: ['Merriweather', 'serif'],
-    },
-    extend: {
-      spacing: {
-        '128': '32rem',
-        '144': '36rem',
-      },
-      borderRadius: {
-        '4xl': '2rem',
-      }
-    }
-  }
->>>>>>> 70a32cfec217aad9e3fb0588ea85e16eac5f689c
 }
