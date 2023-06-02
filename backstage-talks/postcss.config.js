cd4537cbd9025d36244c394c8b71ad31506d519a
Felipe Alves <profelipealves@outlook.com>
2023-06-02 10:35:10 -0300

    fix: deletar todos os diretorios para corregir ambiente

--- backstage-talks/postcss.config.js
+++
@@ -1,9 +0,0 @@
-// postcss.config.js
-module.exports = {
-  plugins: {
-    'postcss-import': {},
-    tailwindcss: {},
-    autoprefixer: {},
-    ...(process.env.NODE_ENV === 'production' ? { cssnano: {} } : {})
-  }
-}
