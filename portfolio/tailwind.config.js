f9317c1e9ead3fb02f4dbab69083a8015db1567c
Felipe Alves <profelipealves@outlook.com>
2023-06-07 07:03:18 -0300

    feat: update config tailwindcss

--- portfolio/tailwind.config.js
+++ portfolio/tailwind.config.js
@@ -2,5 +2,14 @@ module.exports = {
   content: [
     './**/*.{html,js}',
   ],
-  // ...
+  theme: {
+    extend: {
+      colors: {
+        background: '#09090a',
+      },
+      gridTemplateRows: {
+        7: 'repeat(7, minmax(0, 1fr))',
+      },
+    },
+  },
 }
